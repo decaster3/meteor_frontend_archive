dd4888d1699a57447d26dcdab5d2a8fb3b4784b7
Ilgiz <ilgiz1996@mail.ru>
2017-10-06 20:33:43 +0300

    init

--- src/components/start_page/main_start_component.js
+++ src/components/start_page/main_start_component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { pizza_fade_slider_init } from '../../assets/js/slider.js'
 import * as firebase from 'firebase'
+import Products from './start_page_products'
 
 class MainStartPage extends Component {
   constructor(props){
@@ -40,15 +41,19 @@ class MainStartPage extends Component {
 
   render () {
     console.log(this.state.feacherView)
+
     if(this.state.feacherView != null){
       return (
-<header id="header">
-  <div className="header-wrapper container d-flex justify-content-center">
-    <div className="col-12 align-self-center text-center pizza-fade-slider">
-      {this.state.feacherView}
-    </div>
-  </div>
-</header>
+        <div>
+          <header id="header">
+            <div className="header-wrapper container d-flex justify-content-center">
+              <div className="col-12 align-self-center text-center pizza-fade-slider">
+                {this.state.feacherView}
+              </div>
+            </div>
+          </header>
+          <Products/>
+        </div>
       )
     }
     else {
